0dcb1ff4998e8076af1312a88613470910343c29
Ruben Bezuidenhout <rubenb364@gmail.com>
2019-10-31 15:24:03 +0200

    Basic graphql implementation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as graphqlHTTP from "express-graphql";
+import { buildSchema } from "graphql";
 
 let app = express();
 
-app.use(bodyParser);
+let graphMiddleware = graphqlHTTP({
+  schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+  rootValue: {
+    events: () => {
+      return ["Cooking", "Saling", "All-Night Coding"];
+    },
+    createEvent: args => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+});
+
+app.use(bodyParser.json());
+
+app.use("/graphql", graphMiddleware);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
